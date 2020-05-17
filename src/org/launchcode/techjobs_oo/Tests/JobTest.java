fb46ed4b5d5d93e31f07646e17a4b7379924b7bb
Emerald <emerald_mg@yahoo.com>
2020-05-17 18:24:54 -0500

    Part 4 done - check id

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,50 @@
+package org.launchcode.techjobs_oo.Tests;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+public class JobTest {
+    Job testJobOne;
+    Job testJobTwo;
+
+
+    @Before
+    public void createJobObject() {
+        testJobOne = new Job();
+        testJobTwo = new Job();
+
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertFalse(testJobOne.getId() == testJobTwo.getId());
+        assertTrue(testJobTwo.getId() - testJobOne.getId() == 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields () {
+        Job newJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        //assertTrue(newJob.getId() instanceof int);
+        assertTrue(newJob.getName() instanceof String);
+        assertTrue(newJob.getEmployer() instanceof Employer);
+        assertTrue(newJob.getLocation() instanceof Location);
+        assertTrue(newJob.getPositionType() instanceof PositionType);
+        assertTrue(newJob.getCoreCompetency() instanceof CoreCompetency);
+
+
+    }
+
+    @Test
+    public void testJobsForEquality () {
+        Job newJobOne = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job newJobTwo = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(newJobOne.getId() == newJobTwo.getId());
+    }
+}
